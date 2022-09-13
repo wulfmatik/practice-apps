bceefa86232a7bcb8f439bdf34119a322bcfb9b1
Blake Crawford <blake.crawford@outlook.com>
2022-09-12 23:59:29 -0700

    added basic client app index.jsx skeleton and created GlossaryList componenent

---
+++ 1-glossary/client/src/components/GlossaryList.jsx
@@ -0,0 +1,5 @@
+import react from 'react';
+
+class GlossaryList extends React.component {
+  
+};
--- 1-glossary/client/src/index.jsx
+++ 1-glossary/client/src/index.jsx
@@ -1,1 +1,23 @@
+import react from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import GlossaryList from './components/GlossaryList.jsx';
 
+class App extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      glossary: []
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+
+  }
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
